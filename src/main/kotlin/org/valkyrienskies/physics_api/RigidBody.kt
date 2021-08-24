38be6ade2e79dbe1eeacceb1ecfb523f8f3ae5e7
StewStrong <stew.strong.420@gmail.com>
2021-08-23 20:54:54 -0600

    Added RigidBody.setRigidBodyTransform()

--- src/main/kotlin/org/valkyrienskies/physics_api/RigidBody.kt
+++ src/main/kotlin/org/valkyrienskies/physics_api/RigidBody.kt
@@ -1,9 +1,13 @@
 package org.valkyrienskies.physics_api
 
+import org.joml.Quaterniondc
+import org.joml.Vector3dc
+
 interface RigidBody<T : CollisionShape> {
     val rigidBodyId: Int
     val collisionShape: T
     val rigidBodyTransform: RigidBodyTransform
     val inertiaData: RigidBodyInertiaData
     var isStatic: Boolean
+    fun setRigidBodyTransform(position: Vector3dc, rotation: Quaterniondc)
 }
