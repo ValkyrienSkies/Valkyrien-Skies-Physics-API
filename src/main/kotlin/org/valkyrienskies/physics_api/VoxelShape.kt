f08379eac794816d6858ca24674742c12ca9bcc8
StewStrong <stew.strong.420@gmail.com>
2021-08-23 19:28:08 -0600

    Added scaling to VoxelShape

--- src/main/kotlin/org/valkyrienskies/physics_api/VoxelShape.kt
+++ src/main/kotlin/org/valkyrienskies/physics_api/VoxelShape.kt
@@ -1,7 +1,13 @@
 package org.valkyrienskies.physics_api
 
 interface VoxelShape : CollisionShape {
+    /**
+     * The voxel position to local position is defined as the following:
+     *
+     * localPos = (voxelPos + voxelOffset) * scaling
+     */
     fun addVoxel(x: Int, y: Int, z: Int)
     fun removeVoxel(x: Int, y: Int, z: Int)
-    fun setVoxelShapeOffset(xOffset: Double, yOffset: Double, zOffset: Double)
+    fun setVoxelOffset(xOffset: Double, yOffset: Double, zOffset: Double)
+    fun setScaling(scaling: Double)
 }
