f397636020818b54346052df2b3d011d97a23bae
StewStrong <stew.strong.420@gmail.com>
2021-08-23 21:21:54 -0600

    Added gravity to PhysicsWorld.tick()

--- src/main/kotlin/org/valkyrienskies/physics_api/PhysicsWorld.kt
+++ src/main/kotlin/org/valkyrienskies/physics_api/PhysicsWorld.kt
@@ -1,7 +1,9 @@
 package org.valkyrienskies.physics_api
 
+import org.joml.Vector3dc
+
 interface PhysicsWorld {
-    fun tick(timeStep: Double)
+    fun tick(gravity: Vector3dc, timeStep: Double)
     fun createVoxelRigidBody(): VoxelRigidBody
     fun addRigidBody(rigidBody: RigidBody<*>)
     fun removeRigidBody(rigidBody: RigidBody<*>)
