7fcded78b0934a73f3100e2ac59e3ada654799ae
StewStrong <stew.strong.420@gmail.com>
2022-01-09 02:14:12 -0700

    Made RigidBodyInertiaData a data class

--- src/main/kotlin/org/valkyrienskies/physics_api/RigidBodyInertiaData.kt
+++ src/main/kotlin/org/valkyrienskies/physics_api/RigidBodyInertiaData.kt
@@ -2,7 +2,4 @@ package org.valkyrienskies.physics_api
 
 import org.joml.Vector3d
 
-interface RigidBodyInertiaData {
-    var mass: Double
-    var momentOfInertia: Vector3d
-}
+data class RigidBodyInertiaData(val mass: Double, val momentOfInertia: Vector3d)
