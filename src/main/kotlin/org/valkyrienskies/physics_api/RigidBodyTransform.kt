e10ff80cb5b69dec07dbc04702200d5d2d97601c
StewStrong <stew.strong.420@gmail.com>
2022-01-18 00:25:35 -0700

    Added constructors for RigidBodyInertiaData and RigidBodyTransform

--- src/main/kotlin/org/valkyrienskies/physics_api/RigidBodyTransform.kt
+++ src/main/kotlin/org/valkyrienskies/physics_api/RigidBodyTransform.kt
@@ -1,6 +1,14 @@
 package org.valkyrienskies.physics_api
 
+import org.joml.Quaterniond
 import org.joml.Quaterniondc
+import org.joml.Vector3d
 import org.joml.Vector3dc
 
-data class RigidBodyTransform(val position: Vector3dc, val rotation: Quaterniondc)
+data class RigidBodyTransform(val position: Vector3dc, val rotation: Quaterniondc) {
+    companion object {
+        fun createRigidBodyTransform(posX: Double, posY: Double, posZ: Double, rotX: Double, rotY: Double, rotZ: Double, rotW: Double): RigidBodyTransform {
+            return RigidBodyTransform(Vector3d(posX, posY, posZ), Quaterniond(rotX, rotY, rotZ, rotW))
+        }
+    }
+}
