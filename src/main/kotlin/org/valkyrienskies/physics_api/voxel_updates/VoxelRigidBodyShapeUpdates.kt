2cf24cfe66cdc290e4944fa866f56d9afe8cfd65
StewStrong <stew.strong.420@gmail.com>
2022-02-04 01:03:26 -0700

    Simplified VoxelRigidBodyShapeUpdates.kt

--- src/main/kotlin/org/valkyrienskies/physics_api/voxel_updates/VoxelRigidBodyShapeUpdates.kt
+++ src/main/kotlin/org/valkyrienskies/physics_api/voxel_updates/VoxelRigidBodyShapeUpdates.kt
@@ -1,5 +1,22 @@
 package org.valkyrienskies.physics_api.voxel_updates
 
-import org.valkyrienskies.physics_api.RigidBodyReference
+data class VoxelRigidBodyShapeUpdates(val rigidBodyId: Int, val shapeUpdates: Array<IVoxelShapeUpdate>) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
 
-data class VoxelRigidBodyShapeUpdates(val voxelRigidBody: RigidBodyReference, val shapeUpdates: List<IVoxelShapeUpdate>)
+        other as VoxelRigidBodyShapeUpdates
+
+        if (rigidBodyId != other.rigidBodyId) return false
+        if (!shapeUpdates.contentEquals(other.shapeUpdates)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = rigidBodyId
+        result = 31 * result + shapeUpdates.contentHashCode()
+        return result
+    }
+
+}
