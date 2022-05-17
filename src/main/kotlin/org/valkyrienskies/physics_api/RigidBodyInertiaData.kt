e38bce7bd27ffd434122312bb49120cda5ef79ce
StewStrong <stew.strong.420@gmail.com>
2022-05-17 02:08:57 -0600

    Store inverse masses in RigidBodyInertiaData

--- src/main/kotlin/org/valkyrienskies/physics_api/RigidBodyInertiaData.kt
+++ src/main/kotlin/org/valkyrienskies/physics_api/RigidBodyInertiaData.kt
@@ -2,4 +2,4 @@ package org.valkyrienskies.physics_api
 
 import org.joml.Matrix3dc
 
-data class RigidBodyInertiaData(val mass: Double, val momentOfInertia: Matrix3dc)
+data class RigidBodyInertiaData(val invMass: Double, val invMOI: Matrix3dc)
